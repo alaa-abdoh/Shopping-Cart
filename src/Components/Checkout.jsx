45aec64ef3ed372837e14c7f7120c8de9fc50a64
alaa-abdoh <alaajabdoh1701@gmail.com>
2023-10-09 15:04:42 +0300

    handle submit checkout form

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
-
+import Swal from 'sweetalert2'
+import { cartContext } from "../Context/CartProvide"; 
+import { useContext} from "react";
 function Checkout(){
-    let [isCheckout, setIsCheckout] = useState(false)
+    let [isCheckout, setIsCheckout] = useState(false);
+    let {setCart} = useContext(cartContext);
+    function handleSubmit(e){
+        e.preventDefault();
+        setIsCheckout(false);
+        setCart([])
+        Swal.fire({
+            title: 'Sent',
+            text: 'Your order was sent successfully',
+            icon: 'success',
+            confirmButtonText: 'OK',
+            confirmButtonColor:"#99dca8"
+        })
+    }
     return(
        <div className="checkout">
            {  
            isCheckout?
-            <form>
+            <form onSubmit={(e)=>{handleSubmit(e)}}>
                 <h2>Checkout</h2>
-                <input type="text" name="name" id="name" placeholder="Your name" />
-                <input type="text" name="email" id="email" placeholder="Youe Email"/>
+                <input type="text" name="name" id="name" placeholder="Your name" required />
+                <input type="text" name="email" id="email" placeholder="Youe Email" required/>
                 <input type="submit" value="send" />
             </form>
             : <button className="check" onClick={()=>{setIsCheckout(!isCheckout)}}>CheckOut</button>
